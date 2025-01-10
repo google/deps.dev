5a9001f1de24b036f83a20a4bc605a8b6b18f678
Xueqin Cui <cuixq@google.com>
2025-01-10 14:21:25 +1100

    util/resolve: use v3alpha API

--- util/resolve/api.go
+++ util/resolve/api.go
@@ -25,7 +25,7 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
-	pb "deps.dev/api/v3"
+	pb "deps.dev/api/v3alpha"
 	"deps.dev/util/resolve/dep"
 	"deps.dev/util/resolve/version"
 )
