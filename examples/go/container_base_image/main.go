3144611364d2a79167f580dfd9bf41296e7b55da
Josie Anugerah <josieang@google.com>
2025-01-29 11:42:49 +1100

    update documentation

--- examples/go/container_base_image/main.go
+++ examples/go/container_base_image/main.go
@@ -14,10 +14,13 @@
 
 /*
 container_base_image is a simple example application that queries the deps.dev
-API for the base image(s) of an OCI-compliant tarball.
+API for the base image(s) of an Open Container Image (OCI) compliant tarball.
 
-To produce an OCI-compliant tarball, run `docker image save <image id>` with a
-docker client version v25.0 or above.
+The OCI spec is defined at
+https://github.com/opencontainers/image-spec/blob/main/spec.md.
+
+To produce an OCI-compliant tarball using the docker command, run `docker image
+save <image id>` with a docker client v25.0 or above.
 */
 package main
 
@@ -37,84 +40,10 @@ import (
 	"github.com/opencontainers/image-spec/identity"
 )
 
+// response is used to unmarshal QueryContainerImage responseses.
 type response struct {
 	Results []struct {
 		Repository string
-	}
-}
-
-func main() {
-	log.SetFlags(0)
-	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, "Usage: base_image_query <image.tar>\n")
-		flag.PrintDefaults()
-	}
-
-	flag.Parse()
-	if flag.NArg() != 1 {
-		flag.Usage()
-		os.Exit(1)
-	}
-	tarArchive := flag.Arg(0)
-	// Check that the tar declares itself to to be OCI 1.0.0 compliant.
-	var layout ociLayout
-	if err := findFile(tarArchive, "oci-layout", &layout); err != nil {
-		log.Fatalf("%v\nAre you using a docker client version >=25.0 to save the image?", err)
-	}
-	if layout.ImageLayoutVersion != "1.0.0" {
-		log.Fatalf("The oci-layout file lists version %v which is not supported.", layout.ImageLayoutVersion)
-	}
-
-	// Find the manifest(s) for the image. There may be multiple.
-	var idx index
-	if err := findFile(tarArchive, "index.json", &idx); err != nil {
-		log.Fatalf("%v", err)
-	}
-	fmt.Printf("%d manifest(s) found\n", len(idx.Manifests))
-
-	// For each manifest, look up the base image(s).
-	for _, m := range idx.Manifests {
-		// Read the manifest file, which contains the config digest.
-		var mt manifest
-		id, _ := strings.CutPrefix(m.Digest, "sha256:")
-		if err := findFile(tarArchive, fmt.Sprintf("blobs/sha256/%s", id), &mt); err != nil {
-			log.Fatalf("%v", err)
-		}
-		// Read the config file, which contains the diff IDs.
-		var c config
-		id, _ = strings.CutPrefix(mt.Config.Digest, "sha256:")
-		if err := findFile(tarArchive, fmt.Sprintf("blobs/sha256/%s", id), &c); err != nil {
-			log.Fatalf("%v", err)
-		}
-		// For each chain ID, query the deps.dev api to determine
-		// whether it's a known base image.
-		chainIDs := makeChainIDs(c.RootFS.DiffIDs)
-		for i, chainID := range chainIDs {
-			url := "https://api.deps.dev/v3alpha/querycontainerimages/" + chainID
-			resp, err := http.Get(url)
-			if err != nil {
-				log.Fatalf("Request: %v", err)
-			}
-			defer resp.Body.Close()
-			switch resp.StatusCode {
-			case http.StatusNotFound:
-				fmt.Printf("Layer %d: no base image found\n", i)
-			case http.StatusOK:
-				var respBody response
-				err = json.NewDecoder(resp.Body).Decode(&respBody)
-				if err != nil {
-					log.Fatalf("Decoding response body: %v", err)
-				}
-				var repos []string
-				for _, r := range respBody.Results {
-					repos = append(repos, r.Repository)
-				}
-				fmt.Printf("Layer %d: %v\n", i, strings.Join(repos, " "))
-			default:
-				log.Fatalf("Response: %v", resp.Status)
-			}
-
-		}
 	}
 }
 
@@ -144,9 +73,83 @@ type config struct {
 	} `json:"rootfs"`
 }
 
-// findFile looks for the file with the specified filename in the specified
+func main() {
+	log.SetFlags(0)
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: container_base_image <image.tar>\n")
+		flag.PrintDefaults()
+	}
+
+	flag.Parse()
+	if flag.NArg() != 1 {
+		flag.Usage()
+		os.Exit(1)
+	}
+	tarArchive := flag.Arg(0)
+	// Check that the tar declares itself to to be OCI 1.0.0 compliant.
+	var layout ociLayout
+	if err := unmarshalFile(tarArchive, "oci-layout", &layout); err != nil {
+		log.Fatalf("%v\nAre you using a docker client version >=25.0 to save the image?", err)
+	}
+	if layout.ImageLayoutVersion != "1.0.0" {
+		log.Fatalf("The oci-layout file lists version %v which is not supported.", layout.ImageLayoutVersion)
+	}
+
+	// Find the manifest(s) for the image. There may be multiple.
+	var idx index
+	if err := unmarshalFile(tarArchive, "index.json", &idx); err != nil {
+		log.Fatalf("%v", err)
+	}
+	fmt.Printf("%d manifest(s) found\n", len(idx.Manifests))
+
+	// For each manifest, look up the base image(s).
+	for _, m := range idx.Manifests {
+		// Read the manifest file, which contains the config digest.
+		var mt manifest
+		id, _ := strings.CutPrefix(m.Digest, "sha256:")
+		if err := unmarshalFile(tarArchive, fmt.Sprintf("blobs/sha256/%s", id), &mt); err != nil {
+			log.Fatalf("%v", err)
+		}
+		// Read the config file, which contains the diff IDs.
+		var c config
+		id, _ = strings.CutPrefix(mt.Config.Digest, "sha256:")
+		if err := unmarshalFile(tarArchive, fmt.Sprintf("blobs/sha256/%s", id), &c); err != nil {
+			log.Fatalf("%v", err)
+		}
+		// For each chain ID, query the deps.dev api to determine
+		// whether it's a known base image.
+		chainIDs := makeChainIDs(c.RootFS.DiffIDs)
+		for i, chainID := range chainIDs {
+			url := "https://api.deps.dev/v3alpha/querycontainerimages/" + chainID
+			resp, err := http.Get(url)
+			if err != nil {
+				log.Fatalf("Request: %v", err)
+			}
+			switch resp.StatusCode {
+			case http.StatusNotFound:
+				fmt.Printf("Layer %d: no base image found\n", i)
+			case http.StatusOK:
+				var respBody response
+				err = json.NewDecoder(resp.Body).Decode(&respBody)
+				if err != nil {
+					log.Fatalf("Decoding response body: %v", err)
+				}
+				var repos []string
+				for _, r := range respBody.Results {
+					repos = append(repos, r.Repository)
+				}
+				fmt.Printf("Layer %d: %v\n", i, strings.Join(repos, " "))
+			default:
+				log.Fatalf("Response: %v", resp.Status)
+			}
+			resp.Body.Close()
+		}
+	}
+}
+
+// unmarshalFile looks for the file with the specified filename in the specified
 // tarArchive.
-func findFile(tarArchive string, filename string, v any) error {
+func unmarshalFile(tarArchive string, filename string, v any) error {
 	f, err := os.Open(tarArchive)
 	if err != nil {
 		return err
